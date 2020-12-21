805573a98203fd9cf0b53ae9993400a9b29bdfc9
siva <varmadsrk>
2020-12-21 15:05:54 +0000

    no message

--- ShellScripting/13-for.sh
+++ ShellScripting/13-for.sh
@@ -3,3 +3,10 @@
 for i in solaris linus bsd windows macos ; do
     echo "Hello $i"
 done 
+
+OS=(Solaris Linux Bsd Windows Macos)
+index=0
+while [ $index -lt ${#OS[*]} ]; do
+    echo ${OS[$index]}
+    index=$(($index+!))
+done

c3a4ba508c6d2795999b3be67d070b8419d2a5c5
siva <varmadsrk>
2020-12-21 07:31:25 +0000

    commitng python and printing

---
+++ ShellScripting/02-print.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# We can print using two commands , 1. echo, 2. printf
+# My choice of printing is echo command because you save lot of time with syntaxes
+
+echo Hello 
+
+ ## print multi line o/p
+
+ echo Hello
+ echo How are you?
+
+ ### More number of commands will make equallent number of process in the system.
+ ## so that is the reason we will try to minimize the number of commands as much as possible. 
+
+ ## Printing I want to do in single command
+
+ echo Hello How are You?
+ 
+  

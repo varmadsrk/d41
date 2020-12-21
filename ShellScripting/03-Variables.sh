b7a9f1afe75b93b4d6147bdf9360bc09c9b77e0d
siva <varmadsrk>
2020-12-21 08:05:46 +0000

    commit 03-variables.sh

---
+++ ShellScripting/03-Variables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## A name allocated to some content is called as a variable
+
+## Declare variable
+## VAR=DATA
+
+a=10
+b=20.0
+c=hello
+d=hellow world
+
+## Data is not specific to type, Everything is string.
+## Access the variable using $ character preceding to that
+# Ex : $a, $b
+
+echo $a
+echo $d 
+
+# Also you can access a varaible using ${a} in certain situations.
+
+echo $aapples

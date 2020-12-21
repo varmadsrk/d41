9cddad594e27c817c71a59588ad8f1c95f637426
siva <varmadsrk>
2020-12-21 08:58:39 +0000

    no message

--- ShellScripting/06-special-vars.sh
+++ ShellScripting/06-special-vars.sh
@@ -3,3 +3,12 @@
 ## $0-$n , $* , $@ , $# are special variable to access the parsed arguments to the script
 
 echo $0
+
+echo Script Name = $0
+echo First Argument = $1
+
+## Second Argument - $2 , nth argument - $n
+
+echo $*
+
+

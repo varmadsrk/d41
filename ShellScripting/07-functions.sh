6a93e226fd9e8439e9f249b27d54e21b88b1a40f
siva <varmadsrk>
2020-12-21 11:52:17 +0000

    no message

---
+++ ShellScripting/07-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function sample() {
+    echo Hello
+}
+
+#OR
+
+sample2() {
+    echo Hello 2
+}
+
+sample
+sample2
+
+# Good Morning Message
+Welcome_message () {
+    echo "Hello, Good Morning"
+    echo "Welocme to Devops Training"
+}
+
+Welcome_message

2a02aebff54236cecb1d1ea97eb173a551a75e01
siva <varmadsrk>
2020-12-21 07:48:59 +0000

    color coding in 02.print.sh

--- ShellScripting/02-print.sh
+++ ShellScripting/02-print.sh
@@ -31,3 +31,20 @@ echo Hello
 
  ## Printing tab spaces
  echo -e "Name\t\tMarks\nRaju\t\t20\nRamu\t\t30"
+
+ ## We can print some text in color to grab the user attention.
+
+ # Shell has total 6 colors
+ # RGYBMC  - Red, Green, Yellow, Magenta, Cyan
+ # Color            #FG Color           #BG Color
+ # Red                  31                  41
+ # Green                32                  42
+ # Yellow               33                  43
+ # Blue                 34                  44
+ # Magenta              35                  45
+ # Cyan                 36                  46
+
+ # echo -e "\e[COLmHello\e[0m"
+
+ echo -e "\e[31mHello in Red\e[0m"
+ 
